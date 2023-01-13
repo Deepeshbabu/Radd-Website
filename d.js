51fb4c04a32fa135242f6de284a5b470aa63d9b1
Deepeshbabu <104300644+Deepeshbabu@users.noreply.github.com>
2023-01-13 18:59:12 +0530

    Add files via upload

--- d.js
+++ d.js
@@ -1,42 +1,52 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDm-nycdkZQYQi5luYZUkFDFDShw1IbCUM",
+  authDomain: "picnic-backpack-checklis-cfeda.firebaseapp.com",
+  databaseURL: "https://picnic-backpack-checklis-cfeda-default-rtdb.firebaseio.com",
+  projectId: "picnic-backpack-checklis-cfeda",
+  storageBucket: "picnic-backpack-checklis-cfeda.appspot.com",
+  messagingSenderId: "646834676292",
+  appId: "1:646834676292:web:2c25b3f16a35f1ce289211",
+  measurementId: "G-6NB7V49EZH"
+};
 
-if(localStorage.getItem('h') == null){
-    var allkey = [] ;
-}else{
-    var allkey = [localStorage.getItem("h")] ;
-}
-
-console.log(allkey) ;
-
-getdata() ;
+firebase.initializeApp(firebaseConfig);
 
 function pen() 
 {
     var x = document.getElementById("dtp").value ;
     var y = document.getElementById("mdp").value ;
+
+
+    
     var z = document.getElementById("nbp").value ;
     var a = document.getElementById("bwp").value ;
-    console.log(x) ;
-    row = "<tr><td>" + x + "</td><td>" + y + "</td><td>" + z + "</td><td>" + a + "</td></tr>" ;
-    console.log(row) ;
-    document.getElementById("prod").innerHTML = document.getElementById("prod").innerHTML + row ;
-    localStorage.setItem(x, localStorage.getItem(x) + row) ;
-    allkey.push(x) ;
-    console.log(allkey);
-    localStorage.setItem("h",allkey)
-    getdata() ;  
+    firebase.database().ref("x").push({ date : x, model : y, nbun : z, bunwt : a }) ;
+    view() ;
 }
 
-function getdata()
+function getData() { firebase.database().ref("/"+"x").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+  firebase_id = childKey;
+  data = childData;
+//Start code
+console.log(firebase_id);
+console.log(data);
+row = "<tr><th>Date</th><th>Model</th><th>No. Of Bundles</th><th>Bundle Weight</th></tr>" + "<tr><td>"+ data['date'] +"</td><td>"+ data['model'] +"</td><td>"+ data['nbun'] +"</td><td>"+ data['bunwt'] +"</td></tr>";
+document.getElementById("display").innerHTML += row ;
+//End code
+} });  }); }
+
+function view()
 {
-  document.getElementById("pv").style.display = "block"
-  document.getElementById("pe").style.display = "none"
-  document.getElementById("prod").innerHTML = "<tr><th>Date</th><th>Model</th><th>No. Of Bundles</th><th>Bundle Weight</th></tr>" ;
-  console.log("<tr><th>Date</th><th>Model</th><th>No. Of Bundles</th><th>Bundle Weight</th></tr>") ;
+  document.getElementById("pe").style.display = "none" ;
+  document.getElementById("pv").style.display = "block" ;
+  getData() ;
 }
 
 function ope() 
 {
-    document.getElementById("pe").style.display = "block"
+    document.getElementById("pe").style.display = "block" ;
+    document.getElementById("pv").style.display = "none" ;
+    
 }
 
 function myAccFunc(id) {
--- radd.js
+++ radd.js
@@ -1,18 +1,5 @@
-
-    function w3_open() {
-      document.getElementById("main").style.marginLeft = "20%";
-      document.getElementById("mySidebar").style.width = "20%";
-      document.getElementById("mySidebar").style.display = "block";
-      document.getElementById("openNav").style.display = 'none';
-    }
-    function w3_close() {
-      document.getElementById("main").style.marginLeft = "0%";
-      document.getElementById("mySidebar").style.display = "none";
-      document.getElementById("openNav").style.display = "block";
-    }
-
-
 var slideIndex = 1;
+
 showDivs(slideIndex);
 
 function plusDivs(n) {
